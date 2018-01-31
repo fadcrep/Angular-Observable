c002cd2a4617ee71f4ff015024f65f4a408970b3
Crepin Fadjo <crepin.fadjo@qanbio.com>
2018-01-31 16:47:13 +0100

    All to know on Angular Obsservable

---
+++ src/app/app.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
+import { UserService } from 'app/users.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  user1Activated=false;
+  user2Activated=false;
+
+  constructor(private userService:UserService){
+
+  }
+
+
+  ngOnInit(){
+    this.userService.userActivated.subscribe(
+      (id:number)=>{
+
+        if(id===1){
+          this.user1Activated=true;
+        }else  if(id===2){
+          this.user2Activated=true;
+
+        }
+
+      }
+    );
+
+  }
+}
